de45ba5b1405cf53c515cfd1128fd23f819b9ee3
Thomas Leonard <talex5@gmail.com>
2020-12-23 10:07:08 +0000

    Replace custom logging with Prometheus defaults
    Also, use `Cmdliner.Term.term_result` for correct error handling
    (see https://github.com/dbuenzli/cmdliner/pull/124).

--- src/main.ml
+++ src/main.ml
@@ -1,7 +1,7 @@
 (* This is the main entry-point for the executable.
    Edit [cmd] to set the text for "--help" and modify the command-line interface. *)
 
-let () = Logging.init ()
+let () = Prometheus_unix.Logging.init ()
 
 let program_name = "example"
 
@@ -9,7 +9,7 @@ let main config mode repo =
   let repo = Current_git.Local.v (Fpath.v repo) in
   let engine = Current.Engine.create ~config (Pipeline.v ~repo) in
   let site = Current_web.Site.(v ~has_role:allow_all) ~name:program_name (Current_web.routes engine) in
-  Logging.run begin
+  Lwt_main.run begin
     Lwt.choose [
       Current.Engine.thread engine;  (* The main thread evaluating the pipeline. *)
       Current_web.run ~mode site;    (* Optional: provides a web UI *)
@@ -31,7 +31,7 @@ let repo =
 
 let cmd =
   let doc = "an OCurrent pipeline" in
-  Term.(const main $ Current.Config.cmdliner $ Current_web.cmdliner $ repo),
+  Term.(term_result (const main $ Current.Config.cmdliner $ Current_web.cmdliner $ repo)),
   Term.info program_name ~doc
 
 let () = Term.(exit @@ eval cmd)
